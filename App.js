72768534ba0a812ad202c3fa1e30efed694cb779
Berei Dániel <berei.daniel@gmail.com>
2020-01-21 11:36:56 +0100

    basic navigation works

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import ListOfDevices from './components/ListOfDevices';
 import DeviceDetails from './components/DeviceDetails';
+import ListOfDevices from './components/ListOfDevices';
 
-const MainNavigator = createStackNavigator({
-  ListOfDevices: { screen: ListOfDevices },
-  DeviceDetails: { screen: DeviceDetails },
+const App = createStackNavigator({
+  ListOfDevices: {
+    screen: ListOfDevices,
+  },
+  Details: {
+    screen: DeviceDetails
+  }
 });
 
-const App = createAppContainer(MainNavigator);
-
-export default App;
+export default createAppContainer(App);
--- components/ListOfDevices/ListOfDevices.js
+++ components/ListOfDevices/ListOfDevices.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { SafeAreaView, ScrollView, View, Text, Button, Alert } from 'react-native';
 
-const ListOfDevices = () => {
+const ListOfDevices = (props) => {
+  const { navigation } = props;
+  const navigateToDeviceDetails = () => {
+    navigation.navigate('Details');
+  }
   return (
     <>
-    <SafeAreaView>
+      <SafeAreaView>
         <ScrollView>
           <Text>List of devices</Text>
+          <Button
+            title="Go to details"
+            onPress={() => navigateToDeviceDetails()}
+          />
         </ScrollView>
       </SafeAreaView>
     </>
